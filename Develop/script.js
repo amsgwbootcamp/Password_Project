8d23cb598f78ed84d36a158c5f09afdf774eece7
Angela Stevenson <amsgwbootcamp@gmail.com>
2020-02-22 09:33:28 -0500

    Working prompts for numbers, letters, special characters

--- Develop/script.js
+++ Develop/script.js
@@ -2,17 +2,28 @@
 var generateBtn = document.querySelector("#generate");
 var passwordNotValid = true;
 
-function askForLowercase() 
-{
+function askForLowercase() {
   // Ask if they want lowercase letters in their password. 
-  
   var includeLowercase = prompt("Do you want lowercase letters in your password?" + "\n" + "Enter a 'Y' for yes and 'N' for no.");
-  alert("You entered the following for Lowercase: " + includeLowercase);
+  /* alert("You entered the following for Lowercase: " + includeLowercase);  */
+}
+
+function askForUppercase() {
+  // Ask if they want uppercase letters in their password. 
+  
+  var includeUppercase = prompt("Do you want uppercase letters in your password?" + "\n" + "Enter a 'Y' for yes and 'N' for no.");
+  /* alert("You have entered the following for Uppercase: " + includeUppercase);  */
   
 }
 
-function getPasswordLength() 
-{  /*  function getPasswordLength()      */
+function askForNumbers() {
+  // Ask if they want numbers in their password. 
+  
+  var includeNumbers = prompt("Do you want numbers in your password?" + "\n" + "Enter a 'Y' for yes and 'N' for no.");
+  /* alert("You have entered the following for numbers: " + includeNumbers);    */
+  
+}
+function getPasswordLength() {  /*  function getPasswordLength()      */
   // Prompt for length of password
   // Check the value to be greater than 8 and less than or equal to 128
   while (passwordNotValid) 
@@ -37,17 +48,15 @@ function getPasswordLength()
   }  /* end of while */
 }   /* end of function getPasswordLength  */
 
-
-
 // Write password to the #password input
-function writePassword() 
-{
+function writePassword() {
   getPasswordLength();
   askForLowercase();
+  askForUppercase();
+  askForNumbers();
   alert("Back in writePassword");
   /* var password = generatePassword();  */
   var passwordText = document.querySelector("#password");
-
   passwordText.value = password;
 }
 
